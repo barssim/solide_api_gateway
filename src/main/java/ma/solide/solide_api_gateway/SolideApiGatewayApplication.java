e91e9e2d8abcb351cf8ee350e218dfc796dfa9a8
Ahmed_Ubuntu <qarfaoui@gmail.com>
2024-12-21 08:26:19 +0100

    Refactoring

--- src/main/java/ma/solide/solide_api_gateway/SolideApiGatewayApplication.java
+++ src/main/java/ma/solide/solide_api_gateway/SolideApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package solide_api_gateway;
+package ma.solide.solide_api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
